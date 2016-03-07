dc08357b81dc8a91d5606ccefe29e37dd66e12d7
Alexandru Coman <acoman@cloudbasesolutions.com>
2016-03-07 17:31:05 +0200

    Update the install script

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #! /usr/bin/env python
+# *-* encoding: UTF-8 *-*
+"""
+Setup script for the optinum package.
+"""
+
 try:
     from setuptools import setup
 except ImportError:
@@ -11,10 +16,11 @@ setup(
         "This project provides the user with an analysis regarding "
         "the results obtained by a couple of algorithms."),
     long_description=open("README.md").read(),
-    author=("Alexandru Coman"),
-    author_email=("Alexandru Coman <alex@ropython.org>"),
-    url="https://github.com/alexandrucoman/optinum",
+    author=("Alexandru Coman, Alexandru Cîtea"),
+    author_email=("Alexandru Coman <contact@alexcoman.com>, "
+                  "Alexandru Cîtea <alex@citea.ro>"),
+    url="https://github.com/c-square/optinum",
     packages=["optinum"],
     scripts=["scripts/optinum"],
-    requires=[]
+    requires=open("requirements.txt").readlines()
 )
